88902fd2c7b511d6d63a584c7bfdf1c2f2bd11f7
Edeviatov <edeviatov@htc-cs.ru>
2019-07-18 12:44:33 +0400

    Initial Commit

---
+++ pictogram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pictogram
+//
+//  Created by Egor Devyatov on 18.07.2019.
+//  Copyright © 2019 Egor Devyatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
